4dfdbdd86518c646dedc6b870ad9145643c1d567
Yusheng Jin <king0133@live.com>
2021-08-13 14:21:46 -0400

    Blog functionality and polish

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -80,6 +80,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'portfoliodb',
         'USER': 'postgres',
+        # CREATE USER djangoeasondb WITH PASSWORD 'Qazqwe123!';
         'PASSWORD': 'Qazqwe123!',
         'HOST': 'localhost',
         'PORT': '5432'
@@ -123,7 +124,13 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
+STATICFILES_DIRS = [
 
+    os.path.join(BASE_DIR, 'portfolio/static/')
+
+]
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATIC_URL = '/static/'
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
